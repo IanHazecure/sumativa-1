e04cf9bb439da2d23fd7101ee80f9499a7c270b3
IanHazecure <136367549+IanHazecure@users.noreply.github.com>
2026-03-29 20:02:08 -0300

    global exception handler
    aun no implmentado

---
+++ citas-medicas-api/src/main/java/com/citas/exception/GlobalExceptionHandler.java
@@ -0,0 +1,22 @@
+package com.citas.exception;
+
+import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<Map<String, Object>> handleGeneralError(Exception ex) {
+        Map<String, Object> response = new HashMap<>();
+
+    }
+
+
+    }
+
