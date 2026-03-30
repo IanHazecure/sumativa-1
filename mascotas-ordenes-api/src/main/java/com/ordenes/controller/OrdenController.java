cd6eee2eaa3ded63026fa24701a01cbe704e6e67
IanHazecure <136367549+IanHazecure@users.noreply.github.com>
2026-03-29 21:12:47 -0300

    update 2

--- mascotas-ordenes-api/src/main/java/com/ordenes/controller/OrdenController.java
+++ mascotas-ordenes-api/src/main/java/com/ordenes/controller/OrdenController.java
@@ -18,6 +18,38 @@ public class OrdenController {
         return ordenService.obtenerTodasLasOrdenes();
     }
 
-   
+    @GetMapping("/{id}")
+    public Orden obtenerOrdenPorId(@PathVariable int id) {
+        return ordenService.obtenerOrdenPorId(id);
+    }
+
+    @PostMapping
+    public Orden crearOrden(@RequestBody Orden orden) {
+        return ordenService.crearOrden(orden.getCliente(), orden.getProducto(), 
+                                       orden.getCantidad(), orden.getPrecio(), 
+                                       orden.getEstado(), orden.getFecha());
+
+    }
+
+////////
+    @PutMapping("/{id}/estado")
     
+    public boolean actualizarEstado(@PathVariable int id, @RequestParam String nuevoEstado) {
+        return ordenService.actualizarEstado(id, nuevoEstado);
+
+    }
+/////////////
+
+    @DeleteMapping
+
+    public boolean eliminarOrden(@RequestHeader int id) {
+        
+        return ordenService.eliminarOrden(id);
+    }
+/////////////////
+    @GetMapping("/estado/{estado}")
+
+    public List<Orden> obtenerOrdenesPorEstado(@PathVariable String estado) {
+        return ordenService.obtenerOrdenesPorEstado(estado);
+    }
 }
